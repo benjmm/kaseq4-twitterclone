655ffd196a4bb4444137b70a37059ca5e90355c4
Benjamin Mark <benjmark@protonmail.com>
2020-05-26 14:30:49 -0400

    adjusted location of template and static files

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -38,10 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication.apps.AuthenticationConfig',
-    'notification.apps.NotificationConfig',
-    'tweet.apps.TweetConfig',
-    'twitteruser.apps.TwitteruserConfig',
+    'authentication',
+    'notification',
+    'tweet',
+    'twitteruser',
 ]
 
 MIDDLEWARE = [
@@ -59,8 +57,7 @@ ROOT_URLCONF = 'twitterclone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_ROOT, 'templates').replace('\\', '/'),
-                 os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static_dev"),)
+
 AUTH_USER_MODEL = 'twitteruser.CustomUser'
 
 LOGIN_URL = '/login/'
