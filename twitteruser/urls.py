071d436c1e232ced5cef35fcfef1ee0300149a88
Benjamin Mark <benjmark@protonmail.com>
2020-05-26 13:57:59 -0400

    basic user details page working

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from twitteruser import views
 
 urlpatterns = [
+    path('user/<int:id>/', views.UserView, name='user'),
 ]
--- twitteruser/views.py
+++ twitteruser/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import CustomUser
 
-# Create your views here.
+
+@ login_required
+def UserView(request, id):
+    html = "user.html"
+    user = CustomUser.objects.get(id=id)
+    return render(request, html, {'user': user, })
