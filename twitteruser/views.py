b6b67121e8abafb5c84fb57ffe6d26bfa5b3dc27
Benjamin Mark <benjmark@protonmail.com>
2020-05-27 15:14:32 -0400

    implemented autologin after register

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.contrib.auth import login, authenticate
 # from django.contrib.auth.decorators import login_required
 from .models import TwitterUser
 from tweet.models import Tweet
@@ -12,13 +13,23 @@ def UserView(request, id):
     tweets = Tweet.objects.filter(author=user).order_by('-date')
     return render(request, html, {'user': user, 'tweets': tweets})
 
+# https://stackoverflow.com/questions/3222549/
+# how-to-automatically-login-a-user-after-registration-in-django
+
 
 def RegisterView(request):
     html = "form.html"
     if request.method == 'POST':
         form = TwitterUserCreationForm(request.POST)
-        form.save()
-        return HttpResponseRedirect(reverse('home'))
+        if form.is_valid():
+            form.save()
+            # messages.info(
+            #     request, "Thanks for registering. You are now logged in.")
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(request, new_user)
+            return HttpResponseRedirect(reverse('home'))
 
     form = TwitterUserCreationForm()
 
