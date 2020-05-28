added0f6904706f865e3a5ee57b458d29e28ae03
Benjamin Mark <benjmark@protonmail.com>
2020-05-28 16:52:37 -0400

    notifications/notify views & urls initial configuration done

--- notification/urls.py
+++ notification/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from notification import views
 
 urlpatterns = [
+    path('notifications/', views.NotificationsView, name='notifications'),
+    path('notify/', views.NotifyView),
 ]
--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from tweet.models import Tweet
+# from twitteruser.models import TwitterUser
 
-# Create your views here.
+
+@ login_required
+def NotificationsView(request):
+    html = 'notifications.html'
+    user = request.user
+    notification_ids = user.notification_set.values_list('id', flat=True)
+    tweets = Tweet.objects.filter(
+        author__in=notification_ids).order_by('-date')[:10]
+    return render(request, html, {'user': user, 'tweets': tweets})
+
+
+@ login_required
+def NotifyView(request):
+    pass
