42f6795dc6c2da1933f21339e62ef549eacbbae0
Benjamin Mark <benjmark@protonmail.com>
2020-05-28 16:41:47 -0400

    notification model created

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from twitteruser.models import TwitterUser
+from tweet.models import Tweet
 
-# Create your models here.
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(TwitterUser, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
