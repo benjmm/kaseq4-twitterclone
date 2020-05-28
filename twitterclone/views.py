b3509d33c7e5e1f5547fb8963cbaf29a604207d6
Benjamin Mark <benjmark@protonmail.com>
2020-05-28 16:29:51 -0400

    updated homepage to show only tweets of followed users

--- twitterclone/views.py
+++ twitterclone/views.py
@@ -13,5 +13,7 @@ def IndexView(request):
 def HomeView(request):
     html = 'home.html'
     user = request.user
-    tweets = Tweet.objects.all().order_by('-date')[:10]
+    following_ids = user.following.values_list('id', flat=True)
+    tweets = Tweet.objects.filter(
+        author__in=following_ids).order_by('-date')[:10]
     return render(request, html, {'user': user, 'tweets': tweets})
