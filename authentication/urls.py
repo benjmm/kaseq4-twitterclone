5a68cf1d137d7b2ce7bc20f5c4bb62e269d1dbdf
Benjamin Mark <benjmark@protonmail.com>
2020-06-08 14:28:00 -0400

    converted LoginView from function to class

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from authentication import views
 
 urlpatterns = [
-    path('login/', views.LoginView),
+    path('login/', views.LoginView.as_view()),
     path('logout/', views.LogoutView),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render, reverse, HttpResponseRedirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import (LoginForm)
+from django.views.generic import View
 
 
-def LoginView(request):
-    if request.method == "POST":
+# def LoginView(request):
+#     if request.method == "POST":
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             data = form.cleaned_data
+#             user = authenticate(
+#                 request, username=data['username'], password=data['password'])
+#             if user:
+#                 login(request, user)
+#                 return HttpResponseRedirect(
+#                     request.GET.get('next', reverse('home')))
+#     form = LoginForm()
+#     return render(request, 'form.html', {'form': form})
+
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'form.html', {'form': form})
+
+    def post(self, request):
         form = LoginForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
@@ -14,8 +33,7 @@ def LoginView(request):
                 login(request, user)
                 return HttpResponseRedirect(
                     request.GET.get('next', reverse('home')))
-    form = LoginForm()
-    return render(request, 'form.html', {'form': form})
+        return render(request, 'form.html', {'form': form})
 
 
 def LogoutView(request):
